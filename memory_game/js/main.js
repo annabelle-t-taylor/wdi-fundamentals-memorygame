5cf7c554b092cefadf8fed14b9367e88a8210637
Annabelle Thomas Taylor <annabelle.t.taylor@gmail.com>
2019-01-14 14:16:57 -0500

    finished game!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,25 +23,47 @@ let cards = [
 
 let cardsInPlay = [];
 
+let flipCard = function(){
+	let cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].image);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
+}
+
 let checkForMatch = function (){
 	if (cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]){
-			alert("You found a match!");
+			document.getElementById('result').textContent = "You found a match!";
 		}
 		else{
-			alert("Sorry, try again.");
+			document.getElementById('result').textContent = "Sorry, try again!";
 		}
 	}
 }
 
-let flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
+let newGame = function(){
+	for (i = 0; i < cards.length; i++){
+		let cardElement = document.getElementsByTagName('img');
+		cardElement[i].setAttribute('src', 'images/back.png');
+	}
 
-	console.log(cards[cardId].image);
-	console.log(cards[cardId].suit);
+	for (i = cardsInPlay.length-1; i >= 0; i--){
+		cardsInPlay.pop();
+	}
+
+	document.getElementById('result').textContent = "";
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+let createBoard = function(){
+	for (i = 0; i < cards.length; i++){
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	let reset = document.getElementById('resetButton');
+	reset.addEventListener('click',newGame);
+}
+
+createBoard();
